9797b1fa39e17e1906153701bc6b6b18ce99f259
kumarb97 <kumarbamankar@gmail.com>
2022-03-03 23:44:22 +0530

    Ability to deque from begining

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,7 +1,7 @@
-/**UC3 : PROBLEM STATEMENT
- *Ability to create a Queue of 56->30->70 - Use LinkedList to do the Queue Operations - Here enqueue will internally call append method on LinkedList.
- *- So 56 will be added first then 30 and
- *then 70 to make 56 on top of the Stac
+/**UC4 : PROBLEM STATEMENT
+ *Ability to create a Deque.
+ *
+ *
  */
 package stack;
 
@@ -15,10 +15,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Queue");
-        Queue queue = new Queue();
-        queue.enque(56);
-        queue.enque(30);
-        queue.enque(70);
+		Queue queue = new Queue();
+		queue.enque(56);
+		queue.enque(30);
+		queue.enque(70);
+		queue.deque();
+		queue.deque();
+		queue.deque();
+	
 
 	}
 
